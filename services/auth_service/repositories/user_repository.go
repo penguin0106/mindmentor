6176b425d8a12278de1038f8e2f388c6951db46b
penguin0106 <yabmaxim85@gmail.com>
2024-05-10 00:59:45 +0300

    NEW

--- services/auth_service/repositories/user_repository.go
+++ services/auth_service/repositories/user_repository.go
@@ -24,7 +24,7 @@ func (repo *UserRepository) Save(user *models.User) error {
 		return err
 	}
 	if existingUser != nil {
-		return errors.New("Пользователь с таким email уже зарегистрирован")
+		return errors.New("пользователь с таким email уже зарегистрирован")
 	}
 
 	// Проверяем уникальность username
@@ -33,10 +33,10 @@ func (repo *UserRepository) Save(user *models.User) error {
 		return err
 	}
 	if existingUsername != nil {
-		return errors.New("Данный логин уже занят, выберите другой")
+		return errors.New("данный логин уже занят, выберите другой")
 	}
 
-	_, err = repo.DB.Exec("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", user.Username, user.Email, user.Password)
+	_, err = repo.DB.Exec("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", user.Username, user.Email, user.Password)
 	if err != nil {
 		return err
 	}
@@ -46,7 +46,7 @@ func (repo *UserRepository) Save(user *models.User) error {
 // FindByEmail ищет пользователя по его email
 func (repo *UserRepository) FindByEmail(email string) (*models.User, error) {
 	var user models.User
-	err := repo.DB.QueryRow("SELECT id, username, email, password FROM users WHERE email = ?", email).Scan(&user.ID, &user.Username, &user.Email, &user.Password)
+	err := repo.DB.QueryRow("SELECT id, username, email, password FROM users WHERE email = $1", email).Scan(&user.ID, &user.Username, &user.Email, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil // Пользователь с таким email не найден
@@ -59,7 +59,7 @@ func (repo *UserRepository) FindByEmail(email string) (*models.User, error) {
 // FindByUsername ищет пользователя по его username
 func (repo *UserRepository) FindByUsername(username string) (*models.User, error) {
 	var user models.User
-	err := repo.DB.QueryRow("SELECT id, username, email, password FROM users WHERE username = ?", username).Scan(&user.ID, &user.Username, &user.Email, &user.Password)
+	err := repo.DB.QueryRow("SELECT id, username, email, password FROM users WHERE username = $1", username).Scan(&user.ID, &user.Username, &user.Email, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil // Пользователь с таким username не найден
@@ -88,5 +88,5 @@ func (repo *UserRepository) Authenticate(identifier, password string) (*models.U
 		return user, nil
 	}
 	// Если пользователь не найден или пароль не совпадает, возвращаем ошибку
-	return nil, errors.New("Неверные учетные данные")
+	return nil, errors.New("неверные учетные данные")
 }
