21fd26d84f96f80639f3da4f28560221c3e6b67a
penguin0106 <yabmaxim85@gmail.com>
2024-06-11 14:08:59 +0300

    Profile Service

--- services/emotions_service/main.go
+++ services/emotions_service/main.go
@@ -11,29 +11,6 @@ import (
 	"net/http"
 )
 
-const (
-	defaultHost     = "database_postgres"
-	defaultPort     = "5432"
-	defaultUser     = "postgres"
-	defaultPassword = "mindmentor"
-	defaultDBName   = "mindmentor"
-)
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, UPDATE, DELETE, PUT, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		if r.Method == "OPTIONS" {
-			http.Error(w, "", http.StatusNoContent)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	// Подключение к базе данных
 	db, err := connectToDatabase()
@@ -57,4 +34,12 @@ func main() {
 
 	// Запуск сервера
 	http.ListenAndServe(":8082", nil)
+}
+
+// connectToDatabase подключается к базе данных и возвращает объект подключения
+func connectToDatabase() (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", defaultHost, defaultPort, defaultUser, defaultPassword, defaultDBName)
+	db, err := sql.Open("postgres", connStr)
+
+	return db, err
 }
